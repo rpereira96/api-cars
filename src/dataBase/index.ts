5b9d335f085976f2e9f7f317e8cb577cd32709a4
rpereira96 <rafinhapereira96@hotmail.com>
2023-08-19 20:55:15 -0300

    feat: inclusao banco de dados/typeorm

---
+++ src/dataBase/index.ts
@@ -0,0 +1,18 @@
+import { createConnection, getConnectionOptions } from 'typeorm';
+
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'database_cars'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
+
+
+// import { createConnection } from "typeorm";
+
+// createConnection();
