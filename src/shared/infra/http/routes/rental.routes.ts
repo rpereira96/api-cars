7ebeceffbfc65435e71c7e43ed0168753c50b17e
rpereira96 <rafinhapereira96@hotmail.com>
2023-09-02 18:28:01 -0300

    feat: listagem de alugueis

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,11 +2,13 @@ import { CreateRentalController } from "@modules/rentals/useCases/createRental/C
 import { Router } from "express"; 
 import { ensureAutheticated } from "../middlewares/ensureAuthenticated";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsBuUseController = new ListRentalsByUserController();
 
 rentalRoutes.post(
   "/",
@@ -18,6 +20,12 @@ rentalRoutes.post(
   "/devolution/:id",
   ensureAutheticated,
   devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+  "/user", 
+  ensureAutheticated, 
+  listRentalsBuUseController.handle
 );
 
 export { rentalRoutes };
