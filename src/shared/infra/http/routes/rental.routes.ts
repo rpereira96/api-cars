7c17f9bc90f63d6ed464285ab678a9e9a44f93ff
rpereira96 <rafinhapereira96@hotmail.com>
2023-09-02 18:04:19 -0300

    feat: endpoint de devolução do carro

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,15 +1,23 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { Router } from "express"; 
 import { ensureAutheticated } from "../middlewares/ensureAuthenticated";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post(
   "/",
   ensureAutheticated,
   createRentalController.handle
+);
+
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAutheticated,
+  devolutionRentalController.handle
 );
 
 export { rentalRoutes };
